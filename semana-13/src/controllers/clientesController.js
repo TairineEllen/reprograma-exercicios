bf5031f338b41c535959c5a3a6ae219715b79a85
TairineEllen <tairine.ellen@gmail.com>
2020-11-03 18:09:22 -0300

    rota POST/clientes adicionada

--- semana-13/src/controllers/clientesController.js
+++ semana-13/src/controllers/clientesController.js
@@ -1,26 +1,35 @@
+const clientes = require('../models/clientes');
+
 const getAll = (req, res) => {
-    console.log("getAll");
-      res.status(200).send("ok");
+  console.log("getAll");
+  res.status(200).send("ok");
 };
 
 const getCompradores = (req, res) => {
-    console.log("getCompradores");
-      res.status(200).send("ok");
+  console.log("getCompradores");
+  res.status(200).send("ok");
 };
 
 const getByCpf = (req, res) => {
-    console.log("getByCpf");
-      res.status(200).send("ok");
+  console.log("getByCpf");
+  res.status(200).send("ok");
 };
 
 const postCliente = (req, res) => {
-    console.log("postCliente");
-      res.status(200).send("ok");
+
+  let cliente = new clientes(req.body);
+  cliente.save(err => {
+    err ? res.status(424).send({ message: err.message }) 
+    : res.status(201).send({
+      status: 'TRUE',
+      message: 'Cliente incluído(a) com sucesso'
+    });
+  });
 };
 
 module.exports = {
-    getAll,
-    getCompradores,
-    getByCpf,
-    postCliente
+  getAll,
+  getCompradores,
+  getByCpf,
+  postCliente
 }
